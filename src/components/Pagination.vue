cd7c6a8a6687b7575042a655572e889c3aa6ad78
minilabmemo <minilabmemo@gmail.com>
2024-03-29 12:08:35 +0800

    Add: productsByCAT by  page

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav aria-label="Page navigation example">
-    <ul class="pagination justify-content-center">
+  <nav aria-label="Page navigation ">
+    <ul class="pagination justify-content-center ">
       <li class="page-item">
         <a class="page-link " href="#" aria-label="Previous" @click.prevent="updatePage(pages.current_page - 1)"
           :class="{ disabled: !pages.has_pre }">
--- src/views/user/ProductsByCAT.vue
+++ src/views/user/ProductsByCAT.vue
@@ -1,36 +1,41 @@
 <template>
   <LoadingOverlay :active="status.isLoading"></LoadingOverlay>
 
-  <nav aria-label="breadcrumb">
-    <ol class="breadcrumb">
-      <li class="breadcrumb-item"><router-link :to="`/product/${$route.params.category}/all`"> {{ category_name
-          }}</router-link>
-      </li>
-      <li v-if="sub_category_name" class="breadcrumb-item ">
-        <router-link :to="`/product/${$route.params.category}/${$route.params.subcategory}`"> {{ sub_category_name
-          }}</router-link>
-      </li>
-    </ol>
-  </nav>
+  <div class="d-flex flex-column gap-4  ">
 
 
-  <div class="row row-cols-2 row-cols-lg-3 ">
+    <nav aria-label="breadcrumb ">
+      <ol class="breadcrumb mb-0 ">
+        <li class="breadcrumb-item" v-if="$route.params.category !== 'all'"><router-link :to="`/product/all/all`"> 全部
+          </router-link>
+        </li>
+        <li class="breadcrumb-item"><router-link :to="`/product/${$route.params.category}/all`"> {{ category_name
+            }}</router-link>
+        </li>
+        <li v-if="sub_category_name" class="breadcrumb-item ">
+          <router-link :to="`/product/${$route.params.category}/${$route.params.subcategory}`"> {{ sub_category_name
+            }}</router-link>
+        </li>
+      </ol>
+    </nav>
 
-    <SaleItem v-for="(item, index) in showItems" :key="index" :item="item" class="col"
-      :path="`${$route.params.category}/${$route.params.subcategory}`">
-    </SaleItem>
-    <div class="col-6 " v-if="!showItems.length && !status.isLoading">
-      <img src="@/assets/images/design/empty200.png" alt="empty200" class="img-fluid ">
+
+    <div class="row row-cols-2 row-cols-lg-3 ">
+
+      <SaleItem v-for="(item, index) in showItems" :key="index" :item="item" class="col"
+        :path="`${$route.params.category}/${$route.params.subcategory}`">
+      </SaleItem>
+      <div class="col-6 " v-if="!showItems.length && !status.isLoading">
+        <img src="@/assets/images/design/empty200.png" alt="empty200" class="img-fluid ">
+      </div>
+
     </div>
 
+
+    <Pagination :pages="pagination" @change-page-num="filterItemsByPage" v-if="showItems.length !== 0">
+    </Pagination>
+
   </div>
-
-
-
-
-
-
-
 
 </template>
 
@@ -39,14 +44,23 @@
 import getAllProducts from '@/utils/mixins/getProductsAll';
 import categories from '@/utils/const/categories'
 import SaleItem from '@/components/user/SaleItem.vue';
+import Pagination from '@/components/Pagination.vue';
 export default {
-  components: {SaleItem},
+  components: {SaleItem, Pagination},
   data() {
     return {
       categories: categories,
       products: [], // all items
-      showItems: [],//filter by category & page
+      catItems: [],//filter by category
+      showItems: [],//filter  page
       filterErr: "",
+      pagination: {
+        total_pages: 1,
+        current_page: 1,
+        has_pre: true,
+        has_next: true,
+      },
+      dataPerPage: 12,
     }
   },
   mixins: [getAllProducts],
@@ -75,8 +89,31 @@ export default {
 
 
 
-      this.showItems = itemByCAT
-    }
+      this.catItems = itemByCAT
+      this.filterItemsByPage()
+    },
+
+    filterItemsByPage(currentPage = 1) {
+      if (!this.catItems) {
+        return
+      }
+
+      this.pagination.current_page = currentPage
+      this.pagination.total_pages = Math.ceil(Object.keys(this.catItems).length / this.dataPerPage);
+
+      this.pagination.has_pre = true;
+      this.pagination.has_next = true;
+      if (this.pagination.current_page === 1) {
+        this.pagination.has_pre = false;
+      }
+
+      if (this.pagination.current_page === this.pagination.total_pages) {
+        this.pagination.has_next = false;
+      }
+      const startIndex = (this.pagination.current_page - 1) * this.dataPerPage;
+      const endIndex = startIndex + this.dataPerPage;
+      this.showItems = this.catItems.slice(startIndex, endIndex);
+    },
   },
   computed: {
 
