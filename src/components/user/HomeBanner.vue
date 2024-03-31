8db614f9b613a66ed0d0bc49229f0115bc39e24c
minilabmemo <minilabmemo@gmail.com>
2024-03-31 08:25:11 +0800

    Add: banner link

--- src/components/user/HomeBanner.vue
+++ src/components/user/HomeBanner.vue
@@ -1,8 +1,18 @@
 <template>
   <div class="row g-0 ">
-    <div class=" col-4"><img src="@/assets/images/design/banner1.png" alt="banner_main" class="img-fluid"></div>
-    <div class=" col-4"><img src="@/assets/images/design/banner_main.png" alt="banner_main" class="img-fluid"></div>
-    <div class="col-4"><img src="@/assets/images/design/banner3.png" alt="banner_main" class="img-fluid"></div>
+    <div class=" col-4">
+      <router-link to="/product/all/all"><img src="@/assets/images/design/banner1.png" alt="banner_main"
+          class="img-fluid"></router-link>
+    </div>
+    <div class=" col-4">
+      <router-link to="/product/all/all"> <img src="@/assets/images/design/banner_main.png" alt="banner_main"
+          class="img-fluid"></router-link>
+
+    </div>
+    <div class="col-4">
+      <router-link to="/product/all/all"> <img src="@/assets/images/design/banner3.png" alt="banner_main"
+          class="img-fluid"></router-link>
+    </div>
   </div>
 
 </template>
