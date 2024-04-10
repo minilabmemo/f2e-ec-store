881a9bdc840ed91b4004eec9115d00ec01e34a1e
minilabmemo <minilabmemo@gmail.com>
2024-04-10 10:54:45 +0800

    Fix: tidy

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
-import { onMounted } from 'vue'
 
-onMounted(() => {
-  console.log(import.meta.env.VITE_API)
-})
 </script>
 
 
