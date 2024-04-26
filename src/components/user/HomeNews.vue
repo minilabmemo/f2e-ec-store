6239810c5c038544d2c5b72aa9a5281cbd709bf0
minilabmemo <minilabmemo@gmail.com>
2024-04-26 09:24:23 +0800

    Fix: newsCarousel ui

--- src/components/user/HomeNews.vue
+++ src/components/user/HomeNews.vue
@@ -5,33 +5,31 @@
     <div class="col-4"><img src="@/assets/img/design/news3.png" class="img-fluid" alt="news"></div>
   </div>
 
-  <div id="newsCarousel" class="carousel slide d-block d-md-none">
-    <div class="carousel-indicators">
-      <button type="button" data-bs-target="#newsCarousel" data-bs-slide-to="0" class="active" aria-current="true"
-        aria-label="Slide 1"></button>
-      <button type="button" data-bs-target="#newsCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-      <button type="button" data-bs-target="#newsCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  <div class="  d-block d-md-none">
+    <div id="newsCarousel" class="carousel slide" data-bs-ride="carousel">
+
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="@/assets/img/design/news1.png" class="d-block w-100 " alt="news">
+        </div>
+        <div class="carousel-item">
+          <img src="@/assets/img/design/news2.png" class="d-block w-100 " alt="news">
+        </div>
+        <div class="carousel-item">
+          <img src="@/assets/img/design/news3.png" class="d-block w-100 " alt="news">
+        </div>
+      </div>
+      <button class="carousel-control-prev" type="button" data-bs-target="#newsCarousel" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button class="carousel-control-next" type="button" data-bs-target="#newsCarousel" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
     </div>
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <img src="@/assets/img/design/news1.png" class="img-fluid" alt="news">
-      </div>
-      <div class="carousel-item">
-        <img src="@/assets/img/design/news2.png" class="img-fluid " alt="news">
-      </div>
-      <div class="carousel-item">
-        <img src="@/assets/img/design/news3.png" class="img-fluid" alt="news">
-      </div>
-    </div>
-    <button class="carousel-control-prev" type="button" data-bs-target="#newsCarousel" data-bs-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Previous</span>
-    </button>
-    <button class="carousel-control-next" type="button" data-bs-target="#newsCarousel" data-bs-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="visually-hidden">Next</span>
-    </button>
   </div>
+
 </template>
 
 <script setup lang="ts">
