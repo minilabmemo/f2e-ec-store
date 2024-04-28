c8f16d07a6ee468a4718b73a5a28f848edf681dc
minilabmemo <minilabmemo@gmail.com>
2024-04-28 09:59:11 +0800

    ♻️ Fix: home to composition setup

--- src/components/user/HomeItems.vue
+++ src/components/user/HomeItems.vue
@@ -17,54 +17,47 @@
 
 </template>
 
-<script>
+<script setup>
 import HomeItem from '@/components/user/HomeItem.vue';
 import coming1 from '@/assets/img/design/coming1.png';
 import coming2 from '@/assets/img/design/coming2.png';
 import coming3 from '@/assets/img/design/coming3.png';
 import categories from '@/utils/config/categories'
-export default {
-  components: {HomeItem},
-  props: {
-    section: String,
-    CAT: String,
-    products: Array,
+import {ref, watchEffect} from 'vue'
+const props = defineProps({
+  section: String,
+  CAT: String,
+  products: Array,
+});
+const filterErr = ref("");
+const showItems = ref({
+  1: {
+    title: `${props.CAT === categories.new.key ? "韓國熱銷 ONLINE - 新品 " : ""}`, src: coming1, color: "orange",
   },
-  data() {
-    return {
-      filterErr: "",
-      showItems: {
-        1: {
-          title: `${this.CAT === categories.new.key ? "韓國熱銷 ONLINE - 新品 12/25" : ""}`, src: coming1, color: "orange",
-        },
-        2: {
-          title: `${this.CAT === categories.new.key ? "秋冬時裝 - 最新流行色" : ""}`, src: coming2, color: "secondary",
-        },
-        3: {title: `${this.CAT === categories.new.key ? "針織罩衫系列 - 簡約氣質" : ""}`, src: coming3, color: "gray", },
-      }
-    }
+  2: {
+    title: `${props.CAT === categories.new.key ? "秋冬時裝 - 最新流行色" : ""}`, src: coming2, color: "secondary",
   },
-  watch: {
-    products(newProducts) {
-      let previewItem = [];
-      let filterProducts = newProducts.filter((item) => item.category.toString().includes(this.CAT))
+  3: {title: `${props.CAT === categories.new.key ? "針織罩衫系列 - 簡約氣質" : ""}`, src: coming3, color: "gray", },
+});
 
-      if (filterProducts.length >= 3) {
-        previewItem = filterProducts.slice(0, 3);
-      }
+watchEffect(() => {
+  let previewItem = [];
+  let filterProducts = props.products ? props.products.filter((item) => item.category.toString().includes(props.CAT)) : [];
 
-      if (previewItem.length !== 0) {
-        this.showItems = previewItem.map(item => ({
-          title: item.title,
-          src: item.imageUrl,
-          id: item.id,
-
-        }));
-      } else {
-        this.filterErr = "無此商品分類"
-      }
-    }
+  if (filterProducts.length >= 3) {
+    previewItem = filterProducts.slice(0, 3);
   }
 
-}
+  if (previewItem.length !== 0) {
+    showItems.value = previewItem.map(item => ({
+      title: item.title,
+      src: item.imageUrl,
+      id: item.id,
+
+    }));
+  } else {
+    filterErr.value = "無此商品分類"
+  }
+});
+
 </script>
