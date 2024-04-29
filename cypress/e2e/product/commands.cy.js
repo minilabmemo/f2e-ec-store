8427a9245307599d0033a78cdde0feb72a067712
minilabmemo <minilabmemo@gmail.com>
2024-04-29 12:02:28 +0800

    ✅ Add: checkout & tidy test

---
+++ cypress/e2e/product/commands.cy.js
@@ -0,0 +1,10 @@
+Cypress.Commands.add('deleteAllItems', () => {
+  cy.visit('/#/product/all/all')
+  cy.url().should('contain', '/#/product/all/all')
+  cy.get('[data-cy="product-item"]').first().click()
+  cy.contains('button', '加入購物車').click({force: true})
+  cy.visit('/#/user/cart/flow')
+  cy.contains('button', '清空購物車').click()
+  cy.contains('.modal button', '清空購物車').click()
+  cy.contains('div', '您的購物車是空的，請先將商品放入購物車。')
+})
---
+++ cypress/e2e/product/product.cy.js
@@ -0,0 +1,51 @@
+import './commands.cy'
+describe('Product By ID Page Test', () => {
+  it('navbar visible.', () => {
+    cy.viewport('samsung-s10')
+    cy.visit('/#/product/all/all')
+    cy.get('.col-sm-3.justify-content-center > #logo').should('be.visible')
+    cy.get('.navbar-toggler').should('be.visible')
+
+    cy.viewport(992, 768)
+    cy.get('.navbar-toggler').should('not.be.visible')
+  })
+  it('product recommendItems exist.', () => {
+    cy.visit('/#/product/all/all')
+    cy.get('[data-cy="product-item"]').first().click()
+    cy.url().should('contain', '/#/product/all/all/id')
+    cy.contains('h4', '您可能也喜歡').should('exist')
+  })
+  it('user save item and save page test.', () => {
+    cy.visit('/#/product/all/all')
+    cy.get('[data-cy="product-item"]').first().click()
+    cy.url().should('contain', '/#/product/all/all/id')
+
+    cy.contains('button', '加入收藏').click()
+    cy.url().should('contain', '/#/product/all/all/id')
+    cy.contains('button', '已收藏').should('be.visible')
+    cy.get("img[alt='save']").first().click({force: true})
+    cy.url().should('contain', '/#/user/saves')
+    cy.get("[data-cy='item']").should('exist')
+    cy.contains('button', '移出收藏').click()
+    cy.contains('div', '無收藏商品').should('exist') // save action can re-work, only one saved
+  })
+  it('user checkout same item confirm.', () => {
+    cy.deleteAllItems()
+    cy.visit('/#/product/all/all')
+    cy.get('[data-cy="product-item"]').first().click()
+    cy.url().should('contain', '/#/product/all/all/id')
+    cy.contains('button', '加入購物車').click()
+    cy.contains('button', '立即結帳').click()
+    cy.contains('.modal-title', '是否加入相同商品').should('be.visible')
+  })
+  it('no same items in carts, checkout then go to cart page', () => {
+    cy.deleteAllItems()
+    cy.visit('/#/product/all/all')
+    cy.get('[data-cy="product-item"]').first().click()
+    cy.url().should('contain', '/#/product/all/all/id')
+    cy.contains('button', '立即結帳').click()
+    cy.get('.modal-title').should('not.be.visible')
+    cy.url().should('contain', '/#/user/cart/flow')
+  })
+
+})
