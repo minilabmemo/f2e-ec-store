13c751176db298e3314cc762352680f557db3f47
minilabmemo <minilabmemo@gmail.com>
2024-04-30 16:30:55 +0800

    Fix temp wait test for github env issue

--- cypress/e2e/product/commands.cy.js
+++ cypress/e2e/product/commands.cy.js
@@ -4,6 +4,8 @@ Cypress.Commands.add('deleteAllItems', () => {
   cy.get('[data-cy="product-item"]').first().click()
   cy.contains('button', '加入購物車').click({force: true})
   cy.visit('/#/user/cart/flow')
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(1000)
   cy.contains('button', '清空購物車').click()
   cy.contains('.modal button', '清空購物車').click()
   cy.contains('div', '您的購物車是空的，請先將商品放入購物車。')
