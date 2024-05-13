8fed409e1a25d419b06882f4eca4b06518fa69f1
minilabmemo <minilabmemo@gmail.com>
2024-05-13 10:06:30 +0800

    Fix: refactor ts

--- src/components/user/UserNav.vue
+++ src/components/user/UserNav.vue
@@ -1,69 +1,87 @@
 <template>
-
-  <div class="w-100 row   justify-content-center  align-items-center ">
-    <div class="col-4 col-xl-4     d-flex justify-content-center  align-items-center">
-      <router-link to="/user/saves" class="nav-link " data-bs-toggle="tooltip" data-bs-placement="top" title="前往收藏清單">
-        <img src="@/assets/img/icons/save.svg" alt="save" class="icon-size">
+  <div class="w-100 row justify-content-center align-items-center">
+    <div class="col-4 col-xl-4 d-flex justify-content-center align-items-center">
+      <router-link
+        to="/user/saves"
+        class="nav-link"
+        data-bs-toggle="tooltip"
+        data-bs-placement="top"
+        title="前往收藏清單"
+      >
+        <img src="@/assets/img/icons/save.svg" alt="save" class="icon-size" />
       </router-link>
     </div>
 
-    <div class="col-4  col-xl-4   ">
-
-      <div class="row  justify-content-center  align-items-center">
-        <router-link to="/user/cart/flow" class="col-12 nav-link " data-bs-toggle="tooltip" data-bs-placement="top"
-          title="前往購物車">
-          <div class="row justify-content-center  align-items-center position-relative ">
-            <div class="col-4  d-flex justify-content-center  align-items-center">
-              <img src="@/assets/img/icons/cart.svg" class="icon-size" alt="cart">
+    <div class="col-4 col-xl-4">
+      <div class="row justify-content-center align-items-center">
+        <router-link
+          to="/user/cart/flow"
+          class="col-12 nav-link"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title="前往購物車"
+        >
+          <div class="row justify-content-center align-items-center position-relative">
+            <div class="col-4 d-flex justify-content-center align-items-center">
+              <img src="@/assets/img/icons/cart.svg" class="icon-size" alt="cart" />
             </div>
 
-            <div class="cart-number  d-flex justify-content-center  align-items-center" v-if="cartTotalQty !== 0">
+            <div
+              class="cart-number d-flex justify-content-center align-items-center"
+              v-if="cartTotalQty !== 0"
+            >
               <div
-                class=" bg-800  text-white  py-0  rounded-1 cart-number-icon-size d-flex justify-content-center  align-items-center">
-                <span v-if="!status.isAddLoading" class="cart-number-font-size  ">{{ cartTotalQty }}</span>
-                <div v-else class="spinner-border spinner-border-sm " role="status">
+                class="bg-800 text-white py-0 rounded-1 cart-number-icon-size d-flex justify-content-center align-items-center"
+              >
+                <span v-if="!status.isAddLoading" class="cart-number-font-size">{{
+                  cartTotalQty
+                }}</span>
+                <div v-else class="spinner-border spinner-border-sm" role="status">
                   <span class="visually-hidden">Loading...</span>
                 </div>
               </div>
-
             </div>
           </div>
         </router-link>
       </div>
-
     </div>
 
-    <div class="col-4  col-xl-4  d-flex justify-content-center  align-items-center">
-      <div class=" dropdown">
-        <button class="btn  btn-sm  border-0   dropdown-toggle" type="button" id="dropdownMenuButton"
-          data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-toggle="dropdown">
-          <img src="@/assets/img/icons/person.svg" alt="person" class="icon-size">
+    <div class="col-4 col-xl-4 d-flex justify-content-center align-items-center">
+      <div class="dropdown">
+        <button
+          class="btn btn-sm border-0 dropdown-toggle"
+          type="button"
+          id="dropdownMenuButton"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false"
+          data-bs-toggle="dropdown"
+        >
+          <img src="@/assets/img/icons/person.svg" alt="person" class="icon-size" />
         </button>
-        <div class="dropdown-menu  dropdown-menu-end " aria-labelledby="dropdownMenuButton">
-          <router-link class="dropdown-item  " to="/user/orders">查詢訂單</router-link>
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+          <router-link class="dropdown-item" to="/user/orders">查詢訂單</router-link>
 
-          <router-link class="dropdown-item " to="/user/info">會員資料</router-link>
-
+          <router-link class="dropdown-item" to="/user/info">會員資料</router-link>
         </div>
       </div>
     </div>
   </div>
-
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue'
-import Dropdown from 'bootstrap/js/dist/dropdown.js';
-import Tooltip from 'bootstrap/js/dist/tooltip.js';
+import Dropdown from 'bootstrap/js/dist/dropdown.js'
+import Tooltip from 'bootstrap/js/dist/tooltip.js'
 import { storeToRefs } from 'pinia'
 import { useCartStore } from '@/stores/cartStore.js'
-const cartStore = useCartStore();
-const { getCart } = cartStore;
-const { cartTotalQty } = storeToRefs(cartStore);
-import statusStore from '@/stores/statusStore';
-const status = statusStore();
+const cartStore = useCartStore()
+const { getCart } = cartStore
+const { cartTotalQty } = storeToRefs(cartStore)
+import statusStore from '@/stores/statusStore'
+const status = statusStore()
 
-getCart()
+getCart(true)
 
 onMounted(() => {
   let dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
@@ -75,7 +93,6 @@ onMounted(() => {
     return new Tooltip(tooltipTriggerEl)
   })
 })
-
 </script>
 
 <style lang="css" scoped>
@@ -83,12 +100,10 @@ onMounted(() => {
   position: absolute;
   top: -50%;
   right: -0.875rem;
-
 }
 
 .cart-number-font-size {
   font-size: clamp(0.875rem, 0.85rem + 0.125vw, 1rem);
-
 }
 
 .cart-number-icon-size {
@@ -97,7 +112,7 @@ onMounted(() => {
 }
 
 .nav-link:hover {
-  opacity: .8;
+  opacity: 0.8;
 }
 
 .spinner-border-sm {
---
+++ src/stores/cartStore.ts
@@ -0,0 +1,109 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+
+import statusStore from './statusStore'
+import { userCartApi, userCartsApi } from '@/utils/config/path'
+
+import fetchAct from '@/utils/methods/fetchAct'
+interface ProductItem {
+  id: string
+  num: number
+}
+interface CartItem {
+  id: string
+  product_id: string
+  qty: number
+  product: ProductItem
+}
+
+export const useCartStore = defineStore('cartStore', () => {
+  const cart = ref({ carts: [] })
+  const status = statusStore()
+  const cartTotalQty = computed(() => {
+    let total = 0
+    if (!cart.value) {
+      return 0
+    }
+    const cartValue = cart.value
+
+    cartValue.carts.forEach((element: CartItem) => {
+      total = total + element.qty
+    })
+
+    return total
+  })
+
+  function getCart(isGlobeLoad?: Boolean) {
+    const url = `${userCartApi}`
+    if (isGlobeLoad) {
+      fetchAct.get(url).then((res: any) => {
+        cart.value = res.data
+      })
+    } else {
+      fetchAct.get(url, { loadStates: ['isGetLoading'] }).then((res: any) => {
+        cart.value = res.data
+      })
+    }
+  }
+
+  function addCartByItem(cart: CartItem) {
+    const url = `${userCartApi}`
+
+    fetchAct
+      .post(url, { data: cart }, { msgTitle: `加入購物車`, loadStates: ['isAddLoading'] })
+      .then((data: any) => {
+        if (data.data && data.data.product_id !== cart.product_id) {
+          console.warn('後端回應資訊有誤 product_id=', data.data.product_id)
+        }
+        getCart()
+      })
+  }
+
+  function updateCart(item: CartItem) {
+    if (item.qty > item.product.num) {
+      alert(`你輸入的數量大於可購買數量${item.product.num},自動更新為最大可購買數量。`)
+      item.qty = item.product.num
+    }
+    const url = `${userCartApi}/${item.id}`
+
+    const putCart = {
+      product_id: item.product_id,
+      qty: item.qty
+    }
+    status.cartLoadingItem = item.id
+    fetchAct.put(url, { data: putCart }, { loadStates: ['isUpdateLoading'] }).then(() => {
+      status.cartLoadingItem = ''
+      getCart()
+    })
+  }
+
+  function removeCartByID(id: string) {
+    if (!id) {
+      console.error('params is empty or invalid.')
+      return
+    }
+
+    const url = `${userCartApi}/${id}`
+    status.cartLoadingItem = id
+    fetchAct.delete(url, { msgTitle: `移除購物車品項` }).then(() => {
+      status.cartLoadingItem = ''
+      getCart()
+    })
+  }
+  function removeAllItems() {
+    const url = `${userCartsApi}`
+    fetchAct.delete(url, { msgTitle: `移除全部購物車品項` }).then(() => {
+      getCart()
+    })
+  }
+  return {
+    cart,
+    status,
+    cartTotalQty,
+    getCart,
+    addCartByItem,
+    updateCart,
+    removeCartByID,
+    removeAllItems
+  }
+})
